a30c06a53aeb19552c3c02cd21afca2001627486
Michelle Nguyen <michellenguyen11239@aol.com>
2021-04-20 23:06:11 -0700

    Added button, search input, and changed name of HTML

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ $(document).ready(function(){ /* Initializes the side bar */
 var slider = $(".slider"); /* Initializes the main slider with these options */
 M.Slider.init(slider, {
     indicators: false,
-    height: 600,
+    height: 550,
     transition: 500,
     interval: 3000,
 });
+
 
