fd6f58525fcb05895a5f3e8ff6f8519e5a2ef86c
David Valderrama <dvalderrama08@gmail.com>
2021-04-22 01:48:45 -0700

    Formatted cards for images

--- Assets/js/photos.js
+++ Assets/js/photos.js
@@ -10,8 +10,32 @@ function fetchPhotos() {
     .then(data => {
         console.log(data)
         for (let i = 0; i < data.length; i++) {
-            let imgElem = $("<img>").addClass("responsive-img").attr("src", data[i].urls.regular);
-            $("#photoContainer").append(imgElem);
+            let cardElem = createImageElems(data[i].urls.regular);
+            $("#photoContainer").append(cardElem);
         }
     });
 };
+
+// TODO: need to resize the images before they get put in the card
+// so that they're uniform and also pull images from different categories
+function createImageElems(url) {
+    let cardContainer = $("<div>").addClass("col s12 m8 l3");
+
+    let cardElem = $("<div>").addClass("card");
+    let imgContainer = $("<div>").addClass("card-image waves-effect waves-block waves-light");
+    let imgElem = $("<img>").addClass("activator").attr("src", url);
+    
+    let linkContainer = $("<div>").addClass("card-action");
+    let likeElem = $("<a>").attr("href", "#").text("Like");
+    let dislikeElem = $("<a>").attr("href", "#").text("Dislike");
+
+    linkContainer.append(likeElem);
+    linkContainer.append(dislikeElem);
+
+    imgContainer.append(imgElem);
+    cardElem.append(imgContainer);
+    cardElem.append(linkContainer);
+
+    cardContainer.append(cardElem);
+    return cardContainer;
+}
