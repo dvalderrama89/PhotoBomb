515a8068efbf92fae2ae36d29751c88eb0623d0a
David Valderrama <dvalderrama08@gmail.com>
2021-04-22 20:14:01 -0700

    Setup like buttons and saves data to local storage

--- Assets/js/photos.js
+++ Assets/js/photos.js
@@ -1,6 +1,6 @@
 /**
  * 
-all slugs
+All slugs
 ------
     wallpapers
     nature
@@ -14,28 +14,76 @@ all slugs
     health
  */
 var slugs = ["nature", "people", "architecture", "fashion", "film"];
+var categoryWeights = {
+    "nature": 100,
+    "people": 100,
+    "architecture": 100,
+    "fashion": 100,
+    "film": 100
+}
+var photoURLs = {
+    "nature": "",
+    "people": "",
+    "architecture": "",
+    "fashion": "",
+    "film": ""
+}
 
+// Runs on page load
 $(function() {
-    fetchPhotos(); // Currently fetches just the nature category
+    localStorage.setItem("weights", JSON.stringify(categoryWeights)); // Sets default weights
+    fetchPhotos("nature"); // Fetches the default category at position 0
 });
 
-function fetchPhotos() {
+// We need to know what category the image is from to weight the category appropriately.
+// Event delegation is used here to get these click events to register to the dynamically
+// created elements
+$(document).on('click','#likeButton',function() {
+    categoryWeights[this.dataset.category] = 100;
+    localStorage.setItem("weights", JSON.stringify(categoryWeights));
+});
+
+$(document).on('click','#dislikeButton',function() {
+    categoryWeights[this.dataset.category] = 0;
+    localStorage.setItem("weights", JSON.stringify(categoryWeights));
+});
+
+function fetchPhotos(slug) {
     let enc = "b2dLS1VTeUdSUWgtU2tVVnpqS1ByX0t5clZZM0Q3eS1GZElwNmV4VVdIQQ=="; // encoded api key
-    let apiURL = `https://api.unsplash.com/topics/nature/photos/?client_id=${atob(enc)}`;
+    let apiURL = `https://api.unsplash.com/topics/${slug}/photos/?client_id=${atob(enc)}`;
+
+    // Checks local storage to see if we already have the photo URL saved and
+    // skips the API call if we do
+    let urlsObj = localStorage.getItem("photoURLs");
+    if (urlsObj) {
+        let parsedObj = JSON.parse(urlsObj);
+        if (parsedObj[slug].length > 0) {
+            console.log("already have the url");
+            let cardElem = createImageElems(parsedObj[slug], slug);
+            $("#photoContainer").append(cardElem);
+            return;
+        }
+    }
+
     fetch(apiURL)
     .then(response => response.json())
     .then(data => {
-        console.log(data)
+        console.log(data) // debug info
         if (data.length > 1) {
             for (let i = 0; i < 1; i++) {
-                let cardElem = createImageElems(data[i].urls.regular);
+                let cardElem = createImageElems(data[i].urls.regular, slug);
                 $("#photoContainer").append(cardElem);
+
+                // Saves urls so we dont have to make duplicate api calls
+                photoURLs[slug] = data[i].urls.regular;
+                localStorage.setItem("photoURLs", JSON.stringify(photoURLs));
             }
         }
     });
 };
 
-function createImageElems(url) {
+// Makes the card element that contains the photo and like/dislike buttons and attaches them to the page
+function createImageElems(url, slug) {
     let cardContainer = $("<div>").addClass("col offset-s6 offset-l6"); // offset is used to center the image
 
     let cardElem = $("<div>").addClass("card");
@@ -43,8 +91,8 @@ function createImageElems(url) {
     let imgElem = $("<img>").addClass("activator").attr("src", url);
     
     let linkContainer = $("<div>").addClass("card-action");
-    let likeElem = $("<a>").attr("href", "#").text("Like");
-    let dislikeElem = $("<a>").attr("href", "#").text("Dislike");
+    let likeElem = $("<a>").attr("href", "#").text("Like").attr("id", "likeButton").attr("data-category", slug);
+    let dislikeElem = $("<a>").attr("href", "#").text("Dislike").attr("id", "dislikeButton").attr("data-category", slug);
 
     linkContainer.append(likeElem);
     linkContainer.append(dislikeElem);
