641a80a6e6786ce91c1a46c30e3515c74565c281
Michelle Nguyen <michellenguyen11239@aol.com>
2021-04-24 12:27:09 -0700

    Added ID's on my pages, added all search files, and updated global script.js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,22 @@
 /* Global script */
-var searchBar = $(".search-bar"); // Targets the search bar
+var navForm =  $("#navForm"); // Targets the nav form
+var sideNavForm = $("#sideForm") // Targets the side form 
 
 $(document).ready(function(){ /* Initializes the side nav bar */
     $('.sidenav').sidenav();
 });
+
+navForm.on("submit", function(e) { // Saves the user input from nav bar into local storage and redirects screen 
+    e.preventDefault();
+    var navSearch = $("#navSearch").val(); 
+    localStorage.setItem("search", navSearch);
+    window.location.replace("pages/search.html");
+})
+
+sideNavForm.on("submit", function(e) { // Saves the user input from side bar into local storage and redirects screen
+    e.preventDefault();
+    var sideSearch = $("#sideSearch").val(); 
+    localStorage.setItem("search", sideSearch);
+    window.location.replace("pages/search.html");
+})
+/* Need to add #navForm, #sideForm, #navSearch, #sideSearch IDs to all HTML pages */
