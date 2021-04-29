201a69bd893f9f550e8b56fc91bb8dd70abe1fd6
Alex DaRe <alexdare93@gmail.com>
2021-04-28 22:06:12 -0700

    mainpicture and saved modified

--- Assets/js/mainpicture.js
+++ Assets/js/mainpicture.js
@@ -1,22 +1,15 @@
-// let temporaryUrl = ["https://images.unsplash.com/photo-1619009642046-c1961c2a73e7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"]
-//     localStorage.setItem("URL", JSON.stringify(temporaryUrl));
-
-// Populates the image in the main card. Pulls the image from local storage.
-// $(document).ready(function() {
     // Pulls image from local storage (under key "URL") and fills in "#image-zone".
     let imgArr = localStorage.getItem("url");
     let imgEl = $(`#image-zone`);
     let tempImg = $(`<img>`).attr(`src`, imgArr).addClass('tempImg').attr(`id`, `current-img`);
         imgEl.append(tempImg);
 
-// } )
-
 // Save button- Saves URL of image to local storage (under URL key). These images will populate the "saved" page.
 $(document).ready(function() {
     $("#like-btn").click(function(e) {
         // console.log("like button works");
         let currentUrl = $("#current-img").attr("src"); 
-        let saveArr = JSON.parse(localStorage.getItem("url")) || [];
+        let saveArr = JSON.parse(localStorage.getItem("Saved")) || [];
         console.log(saveArr);
         saveArr.push(currentUrl);
             localStorage.setItem("Saved", JSON.stringify(saveArr))
@@ -62,7 +55,7 @@ $(document).ready(function() {
     $("#download-btn").click(function(event) {
         event.preventDefault();
         console.log("download button works");
-        let imgArr = JSON.parse(localStorage.getItem("url"))
+        let imgArr = localStorage.getItem("url")
         window.open(`${imgArr}`);
     })
 } )
