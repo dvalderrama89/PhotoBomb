12a280746b8bd41ff4eb0ec890686fd8f0c477ee
Alex DaRe <alexdare93@gmail.com>
2021-04-28 19:35:47 -0700

    modified mainpicture.html to pull from key 'url' in local storage

--- Assets/js/mainpicture.js
+++ Assets/js/mainpicture.js
@@ -2,16 +2,14 @@
 //     localStorage.setItem("URL", JSON.stringify(temporaryUrl));
 
 // Populates the image in the main card. Pulls the image from local storage.
-$(document).ready(function() {
+// $(document).ready(function() {
     // Pulls image from local storage (under key "URL") and fills in "#image-zone".
-    let imgArr = JSON.parse(localStorage.getItem("url")) || [];
+    let imgArr = localStorage.getItem("url");
     let imgEl = $(`#image-zone`);
-    for (let i = 0; i < imgArr.length; i++) {
-        let tempImg = $(`<img>`).attr(`src`, imgArr[i]).addClass('tempImg').attr(`id`, `current-img`);
+    let tempImg = $(`<img>`).attr(`src`, imgArr).addClass('tempImg').attr(`id`, `current-img`);
         imgEl.append(tempImg);
-    }
 
-} )
+// } )
 
 // Save button- Saves URL of image to local storage (under URL key). These images will populate the "saved" page.
 $(document).ready(function() {
@@ -32,32 +30,32 @@ $(document).ready(function() {
 } )
 
 // Share button- Will use the attached API to create a shorter URL that the user can share.
-$(document).ready(function() {
-    $("#send-btn").click(function(event) {
-        event.preventDefault();
-        console.log("send button works");
-        $(function() {
-            const url = new URL("https://t.ly/api/v1/link/shorten");
-            let headers = {
-                "Content-Type": "application/json",
-                "Accept": "application/json",
-            }
-            let body = {
-                "long_url": tempImg,
-                "domain": "https:\/\/t.ly\/",
-                "api_token": atob("TThyWlVFNWNnbXVLMzZkWmJlZlhXbW55a0Jpb1FHcXRaZjZwTDVoNG5WYXp3UG93Mlh1aHZFQ1Y2aHpt")
-            }
-            fetch(url, {
-                method: "POST",
-                headers: headers,
-                body: JSON.stringify(body)
-            })
-                .then(response => response.json())
-                .then(json => console.log(json));
-        });
-    })
+// $(document).ready(function() {
+//     $("#send-btn").click(function(event) {
+//         event.preventDefault();
+//         console.log("send button works");
+//         $(function() {
+//             const url = new URL("https://t.ly/api/v1/link/shorten");
+//             let headers = {
+//                 "Content-Type": "application/json",
+//                 "Accept": "application/json",
+//             }
+//             let body = {
+//                 "long_url": tempImg,
+//                 "domain": "https:\/\/t.ly\/",
+//                 "api_token": atob("TThyWlVFNWNnbXVLMzZkWmJlZlhXbW55a0Jpb1FHcXRaZjZwTDVoNG5WYXp3UG93Mlh1aHZFQ1Y2aHpt")
+//             }
+//             fetch(url, {
+//                 method: "POST",
+//                 headers: headers,
+//                 body: JSON.stringify(body)
+//             })
+//                 .then(response => response.json())
+//                 .then(json => console.log(json));
+//         });
+//     })
 
-} )
+// } )
 
  // Download button- Opens image in a separate window so the user can download.
 $(document).ready(function() {
